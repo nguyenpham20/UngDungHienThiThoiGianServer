a79665d2e68e3a5d5e7012ed27d7c66b280f69ef
nguyen <nguyenp021098@gmail.com>
2020-05-16 09:04:50 +0700

    Bai Tap

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+<?php
+//Hien thi dong chu xin chao Hello Word
+echo "Hello Word";
+//Ngat xuong dong
+echo "<br/>";
+//Hien thi thoi gian cua may chu
+echo date("F j,Y, g:i ");
+?>
+</h1>
+</body>
+</html>
